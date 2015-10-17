3efbdbeddddcdc856ba59d193a38a1e6b2a2ff0b
Kareem <hshot9930@gmail.com>
2015-10-16 20:18:32 -0400

    Create EnergyReduce.php

---
+++ src/HotshotHD/EnergyReduce.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HotshotHD;
+
+use pocketmine\Server;
+
+use pocketmine\scheduler\PluginTask;
+
+use pocketmine\Player;
+
+class EnergyReduce extends PluginTask {
+
+    public function __construct(ExoSuit $plugin, Player $player){
+        parent::__construct($plugin);
+        $this->plugin = $plugin;
+        $this->player = $player;
+		$this->energy = $this->plugin->energy[$this->player->getName()];
+    }
+
+    public function onRun($currentTick){
+			$this->energy--;
+			
+			if($this->energy == 0) {
+				$this->plugin->cancelTask($this->plugin->tasks[new EnergyReduce($this->plugin, $this->player)]);
+			}
+}
+}
+?>
