41ab638fe1da872dea865753d813238643965bf0
Kareem <hshot9930@gmail.com>
2015-10-17 13:42:48 -0400

    Update Energy.php

--- src/HotshotHD/Energy.php
+++ src/HotshotHD/Energy.php
@@ -3,19 +3,21 @@
 namespace HotshotHD;
 
 use pocketmine\Server;
-use pocketmine\scheduler\PluginTask;
-use pocketmine\Player;
 
+use pocketmine\scheduler\PluginTask;
+
+use pocketmine\Player;
 class Energy extends PluginTask {
 
     public function __construct(ExoSuit $plugin, Player $player){
         parent::__construct($plugin);
         $this->plugin = $plugin;
         $this->player = $player;
-		$this->energy = $this->plugin->energy[$this->player->getName()];
-    }
+		}
 
     public function onRun($currentTick){
-        $this->player->sendPopup("Energy:" . $this->energy);
-    }
+		if($this->plugin->config->get("Energy-Popup-Enabled") == "true") {
+		   $this->player->sendPopup("Energy:" . $this->plugin->getEnergy($this->player));
+		}
+	}
 }
